4b413b361769982b48916d61d57b05542057a9df
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-07-17 19:05:34 +0300

    Update products.js

--- src/pages/products.js
+++ src/pages/products.js
@@ -5,21 +5,23 @@ import * as css from "./products.module.css"
 import Layout from "../components/layout"
 
 const ProductsPage = ({ data }) => (
-    <Layout>
-        <h1>Products</h1>
-        <ul className={css.list}>
-            {data.allShopifyProduct.edges.map(({ node }) => (
-                <li key={node.shopifyId}>
-                    <h3>
-                        {/*<Link to={`/products/${node.handle}`}>{node.title}</Link>*/}
-                        {" - "}${node.priceRangeV2.minVariantPrice.amount}
-                    </h3>
-                    <img src={node.featuredImage.originalSrc} alt={node.title} />
-                    <p>{node.description}</p>
-                </li>
-            ))}
-        </ul>
-    </Layout>
+   <> <div>hey</div>
+    {/*<Layout>*/}
+    {/*    <h1>Products</h1>*/}
+    {/*    <ul className={css.list}>*/}
+    {/*        {data.allShopifyProduct.edges.map(({ node }) => (*/}
+    {/*            <li key={node.shopifyId}>*/}
+    {/*                <h3>*/}
+    {/*                    <Link to={`/products/${node.handle}`}>{node.title}</Link>*/}
+    {/*                    {" - "}${node.priceRangeV2.minVariantPrice.amount}*/}
+    {/*                </h3>*/}
+    {/*                <img src={node.featuredImage.originalSrc} alt={node.title} />*/}
+    {/*                <p>{node.description}</p>*/}
+    {/*            </li>*/}
+    {/*        ))}*/}
+    {/*    </ul>*/}
+    {/*</Layout>*/}
+       </>
 )
 
 export default ProductsPage
