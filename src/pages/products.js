0c1c87c1f03956c478056aeb71394bbe4df67a9d
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-07-17 18:59:42 +0300

    Update products.js

--- src/pages/products.js
+++ src/pages/products.js
@@ -11,7 +11,7 @@ const ProductsPage = ({ data }) => (
             {data.allShopifyProduct.edges.map(({ node }) => (
                 <li key={node.shopifyId}>
                     <h3>
-                        <Link to={`/products/${node.handle}`}>{node.title}</Link>
+                        {/*<Link to={`/products/${node.handle}`}>{node.title}</Link>*/}
                         {" - "}${node.priceRangeV2.minVariantPrice.amount}
                     </h3>
                     <img src={node.featuredImage.originalSrc} alt={node.title} />
