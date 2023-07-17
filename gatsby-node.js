6d1c45479ca0bdde8ee1aa675290ebbc83e0251e
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-07-17 18:36:46 +0300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,46 +16,46 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
-
-const path = require(`path`)
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  // Query for all products in Shopify
-  const result = await graphql(`
-    query {
-      allShopifyProduct(sort: { title: ASC }) {
-        edges {
-          node {
-            title
-            featuredImage  {
-              originalSrc
-            }
-            shopifyId
-            handle
-            description
-            priceRangeV2 {
-              maxVariantPrice {
-                amount
-              }
-              minVariantPrice {
-                amount
-              }
-            }
-            status
-          }
-        }
-      }
-    }
-  `)
-  // Iterate over all products and create a new page using a template
-  // The product "handle" is generated automatically by Shopify
-    result.data.allShopifyProduct.edges.forEach(({ node }) => {
-        createPage({
-            path: `/products/${node.handle}`,
-            component: path.resolve(`./src/templates/product.js`),
-            context: {
-                product: node,
-            },
-        })
-    })
-}
+//
+// const path = require(`path`)
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   // Query for all products in Shopify
+//   const result = await graphql(`
+//     query {
+//       allShopifyProduct(sort: { title: ASC }) {
+//         edges {
+//           node {
+//             title
+//             featuredImage  {
+//               originalSrc
+//             }
+//             shopifyId
+//             handle
+//             description
+//             priceRangeV2 {
+//               maxVariantPrice {
+//                 amount
+//               }
+//               minVariantPrice {
+//                 amount
+//               }
+//             }
+//             status
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   // Iterate over all products and create a new page using a template
+//   // The product "handle" is generated automatically by Shopify
+//     result.data.allShopifyProduct.edges.forEach(({ node }) => {
+//         createPage({
+//             path: `/products/${node.handle}`,
+//             component: path.resolve(`./src/templates/product.js`),
+//             context: {
+//                 product: node,
+//             },
+//         })
+//     })
+// }
